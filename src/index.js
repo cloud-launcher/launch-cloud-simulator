4c06326f49f1be9c49ab4b91e8ec928c00157c0c
Blake La Pierre <blakelapierre@gmail.com>
2015-03-06 15:36:52 -0800

    Here we inject the browser-callable API wrappers.
    Stubbed out providers.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import core from 'launch-cloud-core';
+
 import DOWrapper from 'do-wrapper-browser';
 
-module.exports = core({
-  providers: {
-    digitalocean: DOWrapper
-  }
-});
+module.exports = providerConfigs => {
+  if (providerConfigs === undefined || providerConfigs === null) throw Error('Must provide a providerConfigs object!');
+
+  const api = core({
+    providerApis: {
+      aws: null,
+      azure: null,
+      digitalocean: DOWrapper,
+      gce: null,
+      rackspace: null
+    },
+    providerConfigs
+  });
+
+  return api;
+};
