a2b57744ada9f342511441a9efc8e2956c9a6aed
Blake La Pierre <blakelapierre@gmail.com>
2015-03-09 02:19:42 -0700

    Log pass-through

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import core from 'launch-cloud-core';
 
 import DOWrapper from 'do-wrapper-browser';
 
-module.exports = providerConfigs => {
+module.exports = (providerConfigs, log) => {
   if (providerConfigs === undefined || providerConfigs === null) throw Error('Must provide a providerConfigs object!');
 
   const api = core({
@@ -14,7 +14,7 @@ module.exports = providerConfigs => {
       rackspace: null
     },
     providerConfigs
-  });
+  }, log);
 
   return api;
 };
