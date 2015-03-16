8143712599fb95666a687ac3a7af6a6a4d1ecb63
Blake La Pierre <blakelapierre@gmail.com>
2015-03-16 15:39:19 -0700

    Temp GCE machines

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import DOWrapper from 'do-wrapper-browser';
 import request from 'browser-request';
 
 const proxies = {
-  dockerHubApiRoot: 'http://localhost:3408',
-  discoveryEtcdApiRoot: 'http://localhost:3409'
+  // dockerHubApiRoot: 'http://localhost:3408',
+  // discoveryEtcdApiRoot: 'http://localhost:3409'
+  dockerHubApiRoot: 'http://104.154.35.244',
+  discoveryEtcdApiRoot: 'http://23.236.50.60'
 };
 
 module.exports = (providerConfigs, log) => {
